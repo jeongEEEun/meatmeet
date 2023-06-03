f23bc372f03761b2f5b69a421118fa7f82e72ff0
DESKTOP-R7KS3OJ\PARKJEONGEUN <zzeun103@gmail.com>
2023-06-04 02:18:58 +0900

    프로젝트 생성 Gradle, Springboot 2.7.12, Java11

---
+++ src/test/java/meatmeet/meatmeet/MeatmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package meatmeet.meatmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeatmeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
