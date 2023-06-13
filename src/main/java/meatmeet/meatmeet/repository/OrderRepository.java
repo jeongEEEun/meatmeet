7ad745525c66bffd554493860f7ddabafc83ba5a
ssobang <soyeon9100@gmail.com>
2023-06-13 11:46:14 +0900

    repository 추가

--- src/main/java/meatmeet/meatmeet/repository/OrderRepository.java
+++ src/main/java/meatmeet/meatmeet/repository/OrderRepository.java
@@ -1,11 +1,51 @@
 package meatmeet.meatmeet.repository;
 
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import meatmeet.meatmeet.domain.Order;
 
 @Repository
 public class OrderRepository {
-	
-//	Order findByTitle(String title);
+    private final JdbcTemplate jdbcTemplate;
+    
+    public OrderRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+    
+    private RowMapper<Order> orderRowMapper = (rs, rowNum) -> {
+        Order order = new Order();
+        
+        order.setOrderName(rs.getString("order_name"));
+        order.setOrderPhone(rs.getString("order_phone"));
+        order.setOrderAddress(rs.getString("order_address"));
+        order.setOrderRequest(rs.getString("order_request"));
+        
+        return order;
+    };
+    
+    public List<Order> findByOrderName(String orderName) {
+        String sql = "SELECT * FROM orders WHERE order_name = ?";
+        return jdbcTemplate.query(sql, orderRowMapper, orderName);
+    }
+
+    public void save(Order order) {
+        String sql = "INSERT INTO orders (order_name, order_phone, order_address, order_request) VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(sql, order.getOrderName(), order.getOrderPhone(), order.getOrderAddress(), order.getOrderRequest());
+    }
+
+    public Order findById(Long orderId) {
+        String sql = "SELECT * FROM orders WHERE order_id = ?";
+        return jdbcTemplate.queryForObject(sql, orderRowMapper, orderId);
+    }
+
+    public void deleteById(Long orderId) {
+        String sql = "DELETE FROM orders WHERE order_id = ?";
+        jdbcTemplate.update(sql, orderId);
+    }
 }
--- src/main/java/meatmeet/meatmeet/service/OrderService.java
+++ src/main/java/meatmeet/meatmeet/service/OrderService.java
@@ -1,15 +1,43 @@
 package meatmeet.meatmeet.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import meatmeet.meatmeet.domain.Order;
 import meatmeet.meatmeet.repository.OrderRepository;
 
 @Service
 public class OrderService {
-	private final OrderRepository orderRepository;
-	
-	public OrderService(OrderRepository orderRepository) {
-		this.orderRepository = orderRepository;
-	}
-	
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public List<Order> getOrdersByOrderName(String orderName) {
+        return orderRepository.findByOrderName(orderName);
+    }
+
+    public void createOrder(Order order) {
+        // 주문 생성 로직 구현
+        orderRepository.save(order);
+    }
+
+    public Order getOrderById(Long orderId) {
+        // 주문 조회 로직 구현
+        return orderRepository.findById(orderId);
+    }
+
+    public void processPayment(Long orderId, String paymentMethod) {
+        // 결제 처리 로직 구현
+        Order order = orderRepository.findById(orderId);
+        order.setPayment(paymentMethod);
+        orderRepository.save(order);
+    }
+
+    public void cancelOrder(Long orderId) {
+        orderRepository.deleteById(orderId);
+    }
+
 }
